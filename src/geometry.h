767c718c57f3d6591043f4215dad27e14dd95968
tehnar <tehnar5@gmail.com>
2015-03-17 23:16:09 +0300

    Fixed indentation issue

--- src/geometry.h
+++ src/geometry.h
@@ -10,55 +10,55 @@ int sign(long long a);
 template <class T> T sqr(const T &a);
 
 struct Point
-{	
-	int x, y;
+{   
+    int x, y;
 
-	Point();
-	Point(int _x, int _y);
-	void print(FILE *f = stdout) const;
-	void read(FILE *f = stdin);
+    Point();
+    Point(int _x, int _y);
+    void print(FILE *f = stdout) const;
+    void read(FILE *f = stdin);
 
-	Point operator + (const Point &a) const;
-	Point operator - (const Point &a) const;
-	long long operator * (const Point &a) const;
-	long long operator % (const Point &a) const;
-	Point operator * (int a) const;
+    Point operator + (const Point &a) const;
+    Point operator - (const Point &a) const;
+    long long operator * (const Point &a) const;
+    long long operator % (const Point &a) const;
+    Point operator * (int a) const;
 
-	Point& operator += (const Point &a);
-	Point& operator -= (const Point &a);
-	Point& operator *= (int a);
+    Point& operator += (const Point &a);
+    Point& operator -= (const Point &a);
+    Point& operator *= (int a);
 
-	bool operator == (const Point &a) const;
+    bool operator == (const Point &a) const;
 
-	double len() const;
-	long long len2() const;
+    double len() const;
+    long long len2() const;
 };
 
 struct Polygon
 {
-	Polygon(const std::vector <Point> &points);
-	Point& operator [] (int i);
-	const Point& operator [] (int i) const;
-	void print(FILE *f = stdout) const;
-	int size() const;
-	int downmost, upmost;
-	std::vector <Point> points;
+    Polygon(const std::vector <Point> &points);
+    Point& operator [] (int i);
+    const Point& operator [] (int i) const;
+    void print(FILE *f = stdout) const;
+    int size() const;
+    int downmost, upmost;
+    std::vector <Point> points;
 };
 
 struct Segment
 {
-	Segment();
-	Segment(const Point &first, const Point &second);
-	Point first, second;
-	void print(FILE *f = stdout) const;
+    Segment();
+    Segment(const Point &first, const Point &second);
+    Point first, second;
+    void print(FILE *f = stdout) const;
 };
 
 struct Ray
 {
-	Ray();
-	Ray(const Point &start, const Point &dir);
-	Point start, dir;
-	void print(FILE *f = stdout) const;
+    Ray();
+    Ray(const Point &start, const Point &dir);
+    Point start, dir;
+    void print(FILE *f = stdout) const;
 };
 
 std::ostream& operator << (std::ostream &out, const Point &p);
--- src/test.cpp
+++ src/test.cpp
@@ -15,39 +15,39 @@ Polygon({Point(5, 0), Point(4, 1), Point(1, 1), Point(0, 0)})
 
 int main()
 {
-	Polygon a({Point(2, 0), Point(2, 2), Point(0, 2), Point(0, 0)});
-	assert(isInsideLinear(Point(0, 0), a));
-	assert(isInsideLinear(Point(1, 1), a));
-	assert(isInsideLinear(Point(1, 2), a));
-	assert(!isInsideLinear(Point(-1, 2), a));
-	assert(!isInsideLinear(Point(10, 1), a));
-	assert(!isInsideLinear(Point(3, -1), a));
+    Polygon a({Point(2, 0), Point(2, 2), Point(0, 2), Point(0, 0)});
+    assert(isInsideLinear(Point(0, 0), a));
+    assert(isInsideLinear(Point(1, 1), a));
+    assert(isInsideLinear(Point(1, 2), a));
+    assert(!isInsideLinear(Point(-1, 2), a));
+    assert(!isInsideLinear(Point(10, 1), a));
+    assert(!isInsideLinear(Point(3, -1), a));
 
-	assert(isInsideBS1(Point(0, 0), a));
-	assert(isInsideBS1(Point(1, 1), a));
-	assert(isInsideBS1(Point(1, 2), a));
-	assert(!isInsideBS1(Point(-1, 2), a));
-	assert(!isInsideBS1(Point(10, 1), a));
-	assert(!isInsideBS1(Point(3, -1), a));
+    assert(isInsideBS1(Point(0, 0), a));
+    assert(isInsideBS1(Point(1, 1), a));
+    assert(isInsideBS1(Point(1, 2), a));
+    assert(!isInsideBS1(Point(-1, 2), a));
+    assert(!isInsideBS1(Point(10, 1), a));
+    assert(!isInsideBS1(Point(3, -1), a));
 
-	assert(isInsideBS2(Point(0, 0), a));
-	assert(isInsideBS2(Point(1, 1), a));
-	assert(isInsideBS2(Point(1, 2), a));
-	assert(!isInsideBS2(Point(-1, 2), a));
-	assert(!isInsideBS2(Point(10, 1), a));
-	assert(!isInsideBS2(Point(3, -1), a));
+    assert(isInsideBS2(Point(0, 0), a));
+    assert(isInsideBS2(Point(1, 1), a));
+    assert(isInsideBS2(Point(1, 2), a));
+    assert(!isInsideBS2(Point(-1, 2), a));
+    assert(!isInsideBS2(Point(10, 1), a));
+    assert(!isInsideBS2(Point(3, -1), a));
 
-	for (int i = -50; i <= 50; i++)
-		for (int j = -50; j <= 50; j++)
-		{
-		    for (auto &poly : handPolygons)
-			{
-				bool resultLinear = isInsideLinear(Point(i, j), poly);
-				bool resultBS1 = isInsideBS1(Point(i, j), poly);
-				bool resultBS2 = isInsideBS2(Point(i, j), poly);
-				//cerr << i << " " << j << " " << resultLinear << " " << resultBS1 << " " << resultBS2 << endl;
-				assert(resultLinear == resultBS1 && resultBS1 == resultBS2);
-			}
-		
-		} 
+    for (int i = -50; i <= 50; i++)
+        for (int j = -50; j <= 50; j++)
+        {
+            for (auto &poly : handPolygons)
+            {
+                bool resultLinear = isInsideLinear(Point(i, j), poly);
+                bool resultBS1 = isInsideBS1(Point(i, j), poly);
+                bool resultBS2 = isInsideBS2(Point(i, j), poly);
+                //cerr << i << " " << j << " " << resultLinear << " " << resultBS1 << " " << resultBS2 << endl;
+                assert(resultLinear == resultBS1 && resultBS1 == resultBS2);
+            }
+        
+        } 
 }
